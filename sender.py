4cc81fd38eda4f7159c042fd62ee9e3874649d08
matthewknight <knightpmatthew@gmail.com>
2017-08-01 23:02:44 +1200

    Testing with serialisation and fixed Packet declaration

--- sender.py
+++ sender.py
@@ -1,9 +1,27 @@
 import socket
 import sys
+import cPickle as pickle
 
+class Packet(object):
+    def __init__(self, maginco, p_type, seqno, dataLen, data):
+        self.maginco = maginco
+        self.p_type = p_type
+        self.seqno = seqno
+        self.dataLen = dataLen
+        self.data = data
+        
 #Default port values 7642 23501
 def sender():
     socket_in = socket.socket(AF_INET, SOCKET.SOCK_STREAM)
     socket_in.bind(("", 7642))
     socket_out = socket.socket(AF_INET, SOCKET.SOCK_STREAM)
     socket_out.bind(("", 23501))
+    
+def main():
+    trialPacket = Packet(290137812738912, "IP_NET", 0, 0, 0)
+    testFile = open("packet.txt", "w")
+    pickle.dump(trialPacket, testFile)
+
+main()
+    
+    
