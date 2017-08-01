f42ef8ef89978c239af3c60cb816d51f60e7c0af
samboyles1 <sam.boyles42@gmail.com>
2017-08-01 22:20:30 +1200

    Kept tinkering with cmd line checkers, tried to get hosts bound properly

--- reciever.py
+++ reciever.py
@@ -1,10 +1,10 @@
 import socket
 import sys
 
+#Default port numbers to use : 38764 21983
 
 def reciever():
     recieve_in = socket.socket(AF_INET, SOCKET.SOCK_STREAM)
-
+    recieve_in.bind(("", 38764))
     recieve_out = socket.socket(AF_INET, SOCKET.SOCK_STREAM)
-    
-    
+    recieve_out.bind(("", 21983))
--- sender.py
+++ sender.py
@@ -1,8 +1,9 @@
 import socket
 import sys
 
+#Default port values 7642 23501
 def sender():
     socket_in = socket.socket(AF_INET, SOCKET.SOCK_STREAM)
-    
+    socket_in.bind(("", 7642))
     socket_out = socket.socket(AF_INET, SOCKET.SOCK_STREAM)
-    
+    socket_out.bind(("", 23501))
