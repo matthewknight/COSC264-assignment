e6ff1329e76f52e00c0c69c2bef6dcaadbc389a4
Matthew Knight <knightpmatthew@gmail.com>
2017-08-08 23:02:21 +1200

    Try roundtrip message with specific sockets, currently doesnt work

--- receiver.py
+++ receiver.py
@@ -1,14 +1,34 @@
 import socket
 import sys
 
-def client_socket():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#Server
+def receiver_socket(r_IN_Port=42069, r_OUT_Port=42071, destPort=42070):
     host = socket.gethostname()
-    port = 42069
     
-    s.connect((host, port))
-    print(s.recv(1024))
-    s.close
+    r_IN = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    r_IN.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    r_IN.bind((host, r_IN_Port))
     
-
-client_socket()
+    r_OUT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    r_OUT.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    r_OUT.bind((host, r_OUT_Port))
+    
+    print("Initialised")
+    r_IN.listen(5)
+    
+    while True:
+        connection, address = r_IN.accept()
+        print('Got connection from {}'.format(address))
+        
+        #Receives message from sender
+        data = r_IN.recv(1024)
+        if not data:
+            break
+        print("Data to forward back to sender; {}\n".format(data))
+        
+        #Forwards same message back to sender
+        r_OUT.connect((host, destPort)) 
+        r_OUT.send(data)
+        r_OUT.close()    
+    
+receiver_socket()
--- sender.py
+++ sender.py
@@ -1,22 +1,34 @@
 import socket
 
-def server_socket():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#Client
+def sender_socket(s_IN_Port=42070, s_OUT_Port=42071, c_IN_Port=42069, filename=None):
     host = socket.gethostname()
-    port = 42169
     
-    s.bind((host, port))
+    s_IN = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s_IN.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s_IN.bind((host, s_IN_Port))
     
-    message = "You lika deez nutz?"
-    b = bytearray()
-    b.extend(message.encode())
+    s_OUT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s_OUT.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s_OUT.bind((host, s_OUT_Port))
     
+    #c_IN_Port represents destination, in this case receiver
+    s_OUT.connect((host, c_IN_Port))
     
-    s.listen(5)
-    while True:
-        c, addr = s.accept()
-        print('Got connection from {}'.format(addr))
-        c.send(b)
-        c.close()
-        
-server_socket()
+    print("Initialised")
+    
+    messageToSend = b"You lika deez nutz?"
+    print("Sent", repr(messageToSend))
+    s_OUT.send(messageToSend)
+    
+    s_IN.listen(1)
+    
+    data = s_IN.recv(1024)
+    s_IN.close()
+    print("Received", repr(data))
+
+    
+def main(): 
+    sender_socket()
+    
+main()
