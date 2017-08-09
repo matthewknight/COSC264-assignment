19e6d7b16ac69297c97c5363b7769bc97e33442a
Matthew Knight <knightpmatthew@gmail.com>
2017-08-09 16:54:10 +1200

    Working 1 way with packet serialisation

--- receiver.py
+++ receiver.py
@@ -1,34 +1,34 @@
 import socket
 import sys
+import pickle
+from packet import Packet
 
 #Server
-def receiver_socket(r_IN_Port=42069, r_OUT_Port=42071, destPort=42070):
+def receiver_sockets(r_IN_Port=42069, r_OUT_Port=42071, destPort=42070):
     host = socket.gethostname()
     
     r_IN = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     r_IN.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     r_IN.bind((host, r_IN_Port))
     
-    r_OUT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    r_OUT.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    r_OUT.bind((host, r_OUT_Port))
+    print("Initialised, listening.")
     
-    print("Initialised")
     r_IN.listen(5)
     
     while True:
-        connection, address = r_IN.accept()
-        print('Got connection from {}'.format(address))
+        r_OUT, addr = r_IN.accept() 
+        
+        print('Got connection from {}'.format(addr))
         
         #Receives message from sender
-        data = r_IN.recv(1024)
+        data = r_OUT.recv(1024)
+        data = pickle.loads(data)
         if not data:
             break
-        print("Data to forward back to sender; {}\n".format(data))
+        print("Received; {}\n".format(data))
+          
         
-        #Forwards same message back to sender
-        r_OUT.connect((host, destPort)) 
-        r_OUT.send(data)
-        r_OUT.close()    
-    
-receiver_socket()
+def main():
+    receiver_sockets()
+
+main()
--- sender.py
+++ sender.py
@@ -1,7 +1,10 @@
 import socket
+import sys
+import pickle
+from packet import Packet
 
 #Client
-def sender_socket(s_IN_Port=42070, s_OUT_Port=42071, c_IN_Port=42069, filename=None):
+def sender_main(s_IN_Port=42070, s_OUT_Port=42068, destPort=42069, payload=b"mynamajeff"):
     host = socket.gethostname()
     
     s_IN = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,24 +14,20 @@ def sender_socket(s_IN_Port=42070, s_OUT_Port=42071, c_IN_Port=42069, filename=N
     s_OUT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s_OUT.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s_OUT.bind((host, s_OUT_Port))
-    
-    #c_IN_Port represents destination, in this case receiver
-    s_OUT.connect((host, c_IN_Port))
-    
-    print("Initialised")
-    
-    messageToSend = b"You lika deez nutz?"
-    print("Sent", repr(messageToSend))
-    s_OUT.send(messageToSend)
-    
-    s_IN.listen(1)
-    
-    data = s_IN.recv(1024)
-    s_IN.close()
-    print("Received", repr(data))
 
+    s_OUT.connect((host, destPort))
+    
+    print("Connected to {}".format(destPort))
+    
+    bytestreamToSend = pickle.dumps(payload)
+    
+    print("Sent", repr(payload))
+    s_OUT.send(bytestreamToSend)    
+    
     
 def main(): 
-    sender_socket()
+    trialPacket = Packet(290137812738912, "IP_NET", 0, 0, 0)
+    
+    sender_main(42070, 42068, 42069, trialPacket)
     
 main()
