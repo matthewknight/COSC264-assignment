c6f94e00ab64e3e98d0a937aae06a5b126e1d2dc
Matthew Knight <knightpmatthew@gmail.com>
2017-08-11 11:22:48 +1200

    Add getters to Packet declaration

--- packet.py
+++ packet.py
@@ -6,3 +6,18 @@ class Packet(object):
         self.dataLen = dataLen
         self.data = data
         
+    def getMagicNo(self):
+        return self.magicno
+    
+    def getPacketType(self):
+        return self.p_type
+    
+    def getPacketSequence(self):
+        return self.seqno
+    
+    def getDataLength(self):
+        return self.dataLen
+    
+    def getPacketPayload(self):
+        return self.data
+        
