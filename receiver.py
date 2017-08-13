0fdeefe3d1643ec5feadaed0ff166a753f538833
Matthew Knight <knightpmatthew@gmail.com>
2017-08-13 17:50:25 +1200

    OO Implementation

--- receiver.py
+++ receiver.py
@@ -3,32 +3,61 @@ import sys
 import pickle
 from packet import Packet
 
-#Server
-def receiver_sockets(r_IN_Port=42069, r_OUT_Port=42071, destPort=42070):
-    host = socket.gethostname()
-    
-    r_IN = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    r_IN.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    r_IN.bind((host, r_IN_Port))
-    
-    print("Initialised, listening.")
-    
-    r_IN.listen(5)
-    
-    while True:
-        r_OUT, addr = r_IN.accept() 
-        
-        print('Got connection from {}'.format(addr))
-        
-        #Receives message from sender
-        data = r_OUT.recv(1024)
-        data = pickle.loads(data)
-        if not data:
-            break
-        print("Received; {}\n".format(data))
-          
-        
-def main():
-    receiver_sockets()
+import socket
+import sys
+import pickle
+from packet import Packet
 
+#Server
+class Receiver(object):
+    def __init__(self, r_OUT_Port, r_IN_Port):
+        self.host = socket.gethostname()
+        host = self.host
+        
+        #Check for in range ports
+        if (r_OUT_Port < 1024 or r_OUT_Port > 64000):
+            print("r_OUT port out of range")
+            k=input("Any key to exit")
+            exit()
+        
+        if (r_IN_Port < 1024 or r_IN_Port > 64000):
+            print("r_IN port out of range")
+            k=input("Any key to exit")
+            exit()       
+        
+        self.r_IN = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.r_IN.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.r_IN.bind((host, r_IN_Port))
+        print("r_IN successfully initialised/bound")
+            
+    def sendMessage(self, destPort, message):
+        return None
+
+    def receiveMessage(self):
+        print("Listening...")
+    
+        self.r_IN.listen(5)
+        #receivedMessage = False
+        
+        while True:
+            r_OUT, addr = self.r_IN.accept() 
+            
+            print('Got connection from {}'.format(addr))
+            
+            #Receives message from sender
+            data = r_OUT.recv(1024)
+            data = pickle.loads(data)
+            if not data:
+                break
+            print("Received; {}\n".format(data))
+            
+            
+    
+    def getHost(self):
+        return self.host
+
+def main():
+    receiverServer = Receiver(42071, 42069)
+    receiverServer.receiveMessage()    
+    
 main()
--- sender.py
+++ sender.py
@@ -4,30 +4,54 @@ import pickle
 from packet import Packet
 
 #Client
-def sender_main(s_IN_Port=42070, s_OUT_Port=42068, destPort=42069, payload=b"mynamajeff"):
-    host = socket.gethostname()
-    
-    s_IN = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s_IN.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s_IN.bind((host, s_IN_Port))
-    
-    s_OUT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s_OUT.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s_OUT.bind((host, s_OUT_Port))
+class Sender(object):
+    def __init__(self, s_OUT_Port, s_IN_Port):
+        self.host = socket.gethostname()
+        
+        #Check for in range ports
+        if (s_OUT_Port < 1024 or s_OUT_Port > 64000):
+            print("s_OUT port out of range\n")
+            k = input("Any key to exit")
+            exit()
+            
+            
+        if (s_IN_Port < 1024 or s_IN_Port > 64000):
+            print("s_IN port out of range\n")
+            k = input("Any key to exit")
+            exit()            
+        
 
-    s_OUT.connect((host, destPort))
+        self.s_IN = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s_IN.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s_IN.bind((self.host, s_IN_Port))
+
+        self.s_OUT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s_OUT.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s_OUT.bind((self.host, s_OUT_Port))       
     
-    print("Connected to {}".format(destPort))
+    def sendMessage(self, destPort, message):
+        self.s_OUT.connect((self.host, destPort))
+        print("Connected to {}".format(destPort))
+        
+        bytestreamToSend = pickle.dumps(message)
+            
+        print("Sent", repr(message))
+        self.s_OUT.send(bytestreamToSend)       
+        
+        
+        self.s_OUT.close()
+
+        
+        
     
-    bytestreamToSend = pickle.dumps(payload)
+    def receiveMessage(self):
+        return None
     
-    print("Sent", repr(payload))
-    s_OUT.send(bytestreamToSend)    
+    def getHost(self):
+        return self.host
     
-    
-def main(): 
-    trialPacket = Packet(290137812738912, "IP_NET", 0, 0, 0)
-    
-    sender_main(42070, 42068, 42069, trialPacket)
+def main():
+    senderClient = Sender(42068, 42070)
+    senderClient.sendMessage(42069, "deez nuts")
     
 main()
