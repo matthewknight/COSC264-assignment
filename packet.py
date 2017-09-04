f0ec141397f6d8b94ca3fcde5f3eb83d765cf3a5
Matthew Knight <knightpmatthew@gmail.com>
2017-09-04 14:41:13 +1200

    Added packet constructor error checking and reformatted code.

--- packet.py
+++ packet.py
@@ -1,24 +1,40 @@
 class Packet(object):
-    def __init__(self, p_type, seqno, dataLen, data):
-        self.magicno = 0x497E
+    def __init__(self, magic_no, p_type, seq_no, data_len, data):
+        # Magic number
+        self.magic_no = magic_no
+
+        # Packet type must be of dataPacket or acknowledgementPacket
+        data_packet = 0
+        acknowledgement_packet = 1
+        if p_type != data_packet or p_type != acknowledgement_packet:
+            raise Exception("Incorrect packet type")
         self.p_type = p_type
-        self.seqno = seqno
-        self.dataLen = dataLen
+
+        # Sequence value in binary
+        if not all(c in '01' for c in seq_no):
+            raise Exception("Packet non-binary sequence number")
+        self.seq_no = seq_no
+
+        # Data length field, 0 <= x <= 512
+        if data_len < 0 or data_len > 512:
+            raise Exception("Invalid packet data length")
+        self.data_len = data_len
+
+        # Actual packet payload
         self.data = data
-        
-    def getMagicNo(self):
-        return self.magicno
-    
-    def getPacketType(self):
-        #Flag for if connecting packet or final packet (I think??)
+
+    def get_magic_no(self):
+        return self.magic_no
+
+    def get_packet_type(self):
+        # Flag for if connecting packet or final packet (I think??)
         return self.p_type
-    
-    def getPacketSequence(self):
-        return self.seqno
-    
-    def getDataLength(self):
-        return self.dataLen
-    
-    def getPacketPayload(self):
+
+    def get_packet_sequence_no(self):
+        return self.seq_no
+
+    def get_data_len(self):
+        return self.data_len
+
+    def get_packet_payload(self):
         return self.data
-        
